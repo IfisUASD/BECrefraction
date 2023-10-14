a7aff7aeec6f5d7ae1f0f651bcaca8a86d332340
Barry <g.barreiro140@gmail.com>
2023-10-14 00:56:39 -0400

    fix relative path-ing

--- numSol/main.py
+++ numSol/main.py
@@ -1,5 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numSol import update_fields, calculate_refractive_index
+
+# Discretization and grid
+L = 10.0  # Box size
+N = 1000  # Number of grid points
+dx = L / N  # Grid spacing
+x = np.linspace(0, L, N)  # Spatial grid
+dt = 0.01  # Time step
+T = 10.0  # Total siμlation time
+Nt = int(T / dt)  # Number of time steps
+
+# Initialize fields
+#TODO: get the initial values of de φ, A & J
+φ = np.zeros(N, dtype=complex)
+A = np.zeros(N)
+J = np.zeros(N)
+
+# Initialize arrays to store refractive indices
+k_values = np.linspace(0.001, 1.0, 100)
+refractive_indices = np.zeros((len(k_values), Nt))
 
 def main():
+    # Time evolution and refractive index calculation 
+    for i, k in enumerate(k_values):
+        for t in range(Nt):
+            update_fields(φ, A, J, dx, dt)
+            refractive_indices[i, t] = calculate_refractive_index(φ, A, k, dt*t, x, dx)
+
+    # Plot the refractive index as a function of time
+    for i, k in enumerate(k_values):
+        plt.plot(np.arange(Nt)*dt, refractive_indices[i, :], label=f'k = {k:.3f}')
+
+    plt.xlabel('Time')
+    plt.ylabel('Refractive Index')
+    plt.legend()
+    plt.show()
+
     print("hasta ahora todo jevi")
 
 	#main function
--- numSol/numSol.py
+++ numSol/numSol.py
@@ -1,37 +1,19 @@
 import numpy as np
-from scipy.integrate import simps
 from scipy.constants import pi
-import matplotlib.pyplot as plt
-
-# Constants and parameters
-μ = 1.0  # Mass term
-λ = 1.0  # Self-interaction strength
-χ = 1.0  # Interaction strength
-e = 1.0  # Charge of the scalar field (normalized)
-c = 299792458  # Speed of light in m/s
-ℏ = 1.0545718e-34  # Planck's constant in J*s
-ε_0 = 8.854187817e-12  # Permittivity of free space
-
-# Discretization and grid
-L = 10.0  # Box size
-N = 1000  # Number of grid points
-dx = L / N  # Grid spacing
-x = np.linspace(0, L, N)  # Spatial grid
-dt = 0.01  # Time step
-T = 10.0  # Total simulation time
-Nt = int(T / dt)  # Number of time steps
-
-# Initialize fields
-φ = np.zeros(N, dtype=complex)
-A = np.zeros(N)
-J = np.zeros(N)
-
-# Initialize arrays to store refractive indices
-k_values = np.linspace(0.001, 1.0, 100)
-refractive_indices = np.zeros((len(k_values), Nt))
+from scipy.integrate import simps
 
 # Function to update the fields in each time step
-def update_fields():
+def update_fields(φ, A, J, dx, dt):
+
+    # Constants and parameters
+    μ = 1.0  # Mass term
+    λ = 1.0  # Self-interaction strength
+    χ = 1.0  # Interaction strength
+    e = 1.0  # Charge of the scalar field (normalized)
+    c = 299792458  # Speed of light in m/s
+    ℏ = 1.0545718e-34  # Planck's constant in J*s
+    ε_0 = 8.854187817e-12  # Permittivity of free space
+
     # Update J based on φ and A
     J[:] = -2j * e * (φ.conj() * np.gradient(φ, dx) - np.gradient(φ.conj(), dx) * φ) - 2 * e**2 * A * np.abs(φ)**2
     
@@ -40,9 +22,20 @@ def update_fields():
 
     # Update φ based on its equation of motion (Klein-Gordon equation)
     φ += dt * (np.gradient(np.gradient(φ, dx), dx) + (μ**2 - χ) * φ - 2 * λ * np.abs(φ)**2 * φ) / (ℏ * dt)
+
     
 # Function to calculate the refractive index for a given k
-def calculate_refractive_index(k, t):
+def calculate_refractive_index(φ, A, k, t, x, dx):
+
+    # Constants and parameters
+    μ = 1.0  # Mass term
+    λ = 1.0  # Self-interaction strength
+    χ = 1.0  # Interaction strength
+    e = 1.0  # Charge of the scalar field (normalized)
+    c = 299792458  # Speed of light in m/s
+    ℏ = 1.0545718e-34  # Planck's constant in J*s
+    ε_0 = 8.854187817e-12  # Permittivity of free space
+
     k_term = np.exp(-1j * k * x)
     
     A_k = np.fft.fft(A)
@@ -61,17 +54,14 @@ def calculate_refractive_index(k, t):
     
     return n
 
-# Time evolution and refractive index calculation
-for i, k in enumerate(k_values):
-    for t in range(Nt):
-        update_fields()
-        refractive_indices[i, t] = calculate_refractive_index(k, t*dt)
 
-# Plot the refractive index as a function of time
-for i, k in enumerate(k_values):
-    plt.plot(np.arange(Nt)*dt, refractive_indices[i, :], label=f'k = {k:.3f}')
 
-plt.xlabel('Time')
-plt.ylabel('Refractive Index')
-plt.legend()
-plt.show()
+
+
+
+
+
+
+
+
+
